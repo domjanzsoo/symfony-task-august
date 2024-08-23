0477278b44045734c83681dae1c22abc1d505d7b
Zsolt Domjan <domjanzsoo@yahoo.com>
2024-08-23 15:50:33 +0100

    user registation file upload

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,17 +10,33 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\FileUploader;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        FileUploader $fileUploader): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
+        $fileUploader->setTargetDirectory('public/uploads/profile-image');
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $profileImage = $form->get('profileImage')->getData();
+
+            if ($profileImage) {
+                $profileImageName = $fileUploader->upload($profileImage);
+                $user->setProfileImageRoute($profileImageName);
+            } else {
+                $user->setProfileImageRoute('public/uploads/profile-image/default-avatar.jpg');
+            }
+
             // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -29,8 +45,6 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            $user->setProfileImageRoute('');
-
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
@@ -38,8 +52,8 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_list_users');
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView()
+        return $this->render('registration/index.html.twig', [
+            'registerForm' => $form->createView()
         ]);
     }
 }
